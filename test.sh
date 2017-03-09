d17aa20a5e7388df364ee1ef2769251240f96d90
David Rautureau <david.rautureau@sonarsource.com>
2017-03-09 11:10:40 +0100

    Change SHA1 of the attached artifact

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #download SQ
 wget -c https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-6.3-RC4.zip --auth-no-challenge
 
+# Change SHA1
+rm sonarqube-6.3-RC4 sonarqube-6.3-RC4-new.zip -Rf
+unzip sonarqube-6.3-RC4.zip
+zip -r sonarqube-6.3-RC4-new.zip sonarqube-6.3-RC4/
+sha1sum sonarqube-6.3-RC4-new.zip
+
 # set project version
 PROJECT_VERSION=1.$TRAVIS_BUILD_NUMBER
 mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$PROJECT_VERSION
